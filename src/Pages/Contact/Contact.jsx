eb6688300186c4d7d4f7b9d9ccd6267498550de7
Collins-cl <collinsolads@gmail.com>
2023-09-21 13:31:31 +0100

    implemented the contact apis integration

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,9 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Contact/Contact.scss";
 import { FaLinkedin, FaInstagram, FaFacebookF } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Contact = () => {
+  const success = () =>
+    toast.success("Email sent successfully", {
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+
+  const error = () =>
+    toast.error("Something went wrong", {
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+
+  // i used first_name in place of team's name as a result of contradicting var names in api.
+
+  const intialValue = {
+    first_name: "",
+    topic: " ",
+    email: "",
+    message: "",
+  };
+
+  const [state, setState] = useState(intialValue);
+
+  const { first_name, topic, email, message } = state;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setState({ ...state, [name]: value });
+  };
+
+  const baseURL = "https://backend.getlinked.ai";
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .post(`${baseURL}/hackathon/contact-form`, {
+        first_name,
+        topic,
+        email,
+        message,
+      })
+      .then((res) => {
+        if (res.status === 201) {
+          console.log("success");
+          success();
+          setState(intialValue);
+        }
+      })
+      .catch((err) => {
+        error();
+      });
+  };
+
   return (
     <div className="contact">
       <div className="wrapper">
@@ -51,18 +120,46 @@ const Contact = () => {
 
           <p>Email us below to any question related to our event</p>
 
-          <form action="submit">
+          <form action="submit" onSubmit={handleSubmit}>
             <div className="input">
-              <input type="text" placeholder="Team's Name" required />
-              <input type="email" placeholder="Topic" required/>
+              <input
+                type="text"
+                placeholder="Team's Name"
+                name="first_name"
+                value={first_name}
+                onChange={handleChange}
+                required
+              />
 
-              <input type="email" placeholder="Email" />
+              <input
+                type="text"
+                placeholder="Topic"
+                name="topic"
+                value={topic}
+                onChange={handleChange}
+                required
+              />
+
+              <input
+                type="email"
+                placeholder="Email"
+                name="email"
+                onChange={handleChange}
+                value={email}
+              />
             </div>
-            <textarea name="" id="" placeholder="Message"></textarea>
+            <textarea
+              name="message"
+              placeholder="Message"
+              value={message}
+              onChange={handleChange}
+            ></textarea>
             <button type="submit">Submit</button>
           </form>
         </div>
       </div>
+
+      <ToastContainer />
     </div>
   );
 };
